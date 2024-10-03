1387222e81dbea067786afdff6b33ef283b4bd5a
eduardopvieira16 <eduardo_pereirav@outlook.com>
2024-10-03 13:53:07 -0300

    Criação do Hub Link para divulgação dos meus links pessoais.

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+function lightTheme() {
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // definir a imagem diretamente
+  img.setAttribute("src", "./assets/avatar.jpg")
+}
